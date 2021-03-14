2d89570c5630ab161903a7e713aaf84ad18a68c7
omidmirzajani <omidmirzajani82@gmail.com>
2021-03-14 18:36:51 +0330

    add component to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Route, Switch } from 'react-router';
 import HomePage from './HomePage/HomePage';
+import Signup from './Signup/Signup';
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route path='/' exact component={HomePage} />
+        <Route path='/signup' exact component={Signup} />
       </Switch>
     </div>
   );
