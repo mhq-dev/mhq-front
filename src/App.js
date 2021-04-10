1027e4701f1717fabe2c1577ffb270ddc3888943
omidmirzajani <omidmirzajani82@gmail.com>
2021-04-10 12:10:00 +0430

    App.js added for profile and others

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Signup from './Signup/Signup';
 import Login from './Login/Login';
 import Dashboard from './Dashboard/Dashboard'
 import EmailConfirmation from './Signup/EmailVerification';
+import Profile from './Profile/Profile';
+import OtherProfile from './Profile/OtherProfile';
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
         <Route path='/login' exact component={Login} />
         <Route path="/activate/*" exact component={EmailConfirmation} />
         <Route path='/dashboard' exact component={Dashboard}/>
+        <Route path='/Profile' exact component={Profile} />
+        <Route path='/Profile/*' exact component={OtherProfile} />
       </Switch>
     </div>
   );
