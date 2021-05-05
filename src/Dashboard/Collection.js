81c38c16dc92a9bb0b1f708a376f6ed0593de2b7
mohammadhosseinkarimian <47971866+mohammadhosseinkarimian@users.noreply.github.com>
2021-05-05 20:37:24 +0430

    [add] demote to collection

--- src/Dashboard/Collection.js
+++ src/Dashboard/Collection.js
@@ -8,6 +8,7 @@ import CreateCollection from '../CreateCollection/CreateCollection'
 import { Layout,Avatar, Menu, Dropdown , Form, Input, Button ,List,Tabs,Select,Row,Col,Switch,Radio ,message} from "antd";
 import { DownOutlined,ClockCircleOutlined,ApiOutlined,NodeCollapseOutlined,BorderOutlined,CrownOutlined ,EditOutlined ,EyeOutlined,
     FundProjectionScreenOutlined,LogoutOutlined} from '@ant-design/icons';
+import {  FaUnlockAlt,FaLock } from "react-icons/fa";
 import FormItem from 'antd/lib/form/FormItem';
 import {  Link, NavLink } from 'react-router-dom';
 const { TabPane } = Tabs;
@@ -42,7 +43,8 @@ class Collection extends React.Component {
         .then((response)=>{
             if (response.status === 204){
                 message.success("You deleted this collection successfully")
-                this.getthis()
+                window.location.reload();
+
             }
             else{
                 message.error("Please try again")
@@ -58,9 +60,9 @@ class Collection extends React.Component {
         'Authorization' :`Token ${localStorage.getItem('token')}`
       }})
       .then((response)=>{
-          if (response.status === 200){
+          if (response.status === 204){
               message.success("You left this collection successfully")
-              this.getthis()
+              window.location.reload();
           }
           else{
               message.error("Please try again")
@@ -177,6 +179,28 @@ class Collection extends React.Component {
           message.error("Something went wrong.")
       })
       }
+      Demote(user,item){
+        let st="visitor"
+        
+        Axios.put('http://37.152.188.83/api/collection/'+item.id+"/promote_user/"+user.user,{
+          user: user.user,role: st
+      },{headers:{
+        'Content-Type' : 'application/json',
+        'Authorization' :`Token ${localStorage.getItem('token')}`
+      }})
+      .then((response)=>{
+          if (response.status === 200){
+              message.success("demoted successfully")
+              this.getthis()
+          }
+          else{
+              message.error("Please try again")
+          }
+      })
+      .catch((error)=>{
+          message.error("Something went wrong.")
+      })
+      }
     onFinish(v,name,status) {
         let r="public"
               if(this.state.type===false)
@@ -257,19 +281,18 @@ class Collection extends React.Component {
                 <div style={{marginTop: '6%'}}>
         <Row >
 
-          <Col span={8} style={{marginLeft: '5%',alignContent: 'center',alignItems: 'center'}}>
+          <Col span={8} style={{marginLeft: '5%',marginTop: '1%',alignContent: 'center',alignItems: 'center'}}>
             <div className="collectinbox" style={{textAlign: 'left',marginLeft: '10%'}}>
             <h5 style={{fontSize: '23px' ,marginLeft: '5%'}}>Name: {item.name}</h5>
-            <h5 style={{fontSize: '22px',marginLeft: '5%'}}>Status: {item.type}</h5>
-            <h5 style={{fontSize: '22px',paddingRight: '5%',marginLeft: '5%'}}>Users :</h5>
-            <h5 style={{fontSize: '18px',marginLeft: '5%'}}>{this.state.users.map(user=>(user.user+" "))} </h5>
+            <h5 style={{fontSize: '22px',marginLeft: '5%'}}>Status: {item.type==='private'? <FaLock style={{fill: 'gold', fontSize: '20px',marginTop: '-5%'}}/>:<FaUnlockAlt style={{fill: 'gold', fontSize: '20px',marginTop: '-5%'}}/>} {item.type}</h5>
+            <h5 style={{fontSize: '22px',paddingRight: '5%',marginLeft: '5%'}}>Number of users : {this.state.users.length} </h5>
             </div>
             
           </Col>
           <Col span={12}>
             <Row style={{float: 'right',marginTop: '1%'}}>
               <div style={{alignContent: 'center',alignItems: 'center'}}>
-            <h5 style={{fontSize: '22px'}}>Edit name and status:</h5>
+            <h5 style={{fontSize: '22px'}}>Edit name and status</h5>
             <br/>
             <div>
             <Form
@@ -316,35 +339,54 @@ class Collection extends React.Component {
           </Col>
          
         </Row>
+        <Row style={{marginTop: '3%' ,marginLeft: '40%'}}>
+          <Col span={8}>
+            {this.state.power==="owner"?
+              <Button style={{backgroundColor: 'red',color: 'white',border: 'none',width: '90%'}} onClick={()=>{this.deleCollection(item)}}>Delete</Button>
+              :
+              <Button style={{backgroundColor: 'red',color: 'white',border: 'none',width: '90%'}} onClick={()=>{this.leaveCollection(item)}}>Leave</Button>
+            }
+          </Col>
+        </Row>
         <div style={{marginTop: '3%',marginLeft: '5%',width: '90%',borderTop: '1px solid gray'}}>
           <Row >
                 <Col span={2} style={{marginLeft: '1.8%'}}>
-                  <h4 style={{fontSize: '20px'}}>Users</h4>
+                  <h4 style={{fontSize: '20px',marginTop: '1%'}}>Users</h4>
                 </Col>
           </Row>
           <Row>
           {this.state.users.map(user=>(
             
             <Col span={8}>
-              <Row>
+              <Row style={{marginTop: '3%'}}>
                 <Col span={12} style={{float: 'right'}}>
-                <Avatar src={Pro} style={{width: '10vw',height: '10vw'}}></Avatar>
+                {user.avatar===null?<div className="container"> <Link to={"/Profile/"+user.user}>
+                <Avatar className="avatar_image" src={Pro}  style={{width: '11vw',height: '11vw',marginTop: '-14%'}}></Avatar><div class="overlay"><div className="imagetext">view</div></div></Link>
+                </div>:<div className="container"><Link to={"/Profile/"+user.user}><Avatar className="avatar_image" src={user.avatar}style={{width: '7vw',height: '7vw'}}></Avatar><div class="overlay"><div className="imagetext">view</div></div></Link></div>} 
                 </Col>
                 <Col span={12} style={{alignContent: 'center',alignItems: 'center'}}>
-                <div className="collectinbox" style={{textAlign: 'left',marginLeft: '-1%',border: 'none',marginTop: '10%'}}>
-                <h5 style={{fontSize: '16px' }}>{user.user}</h5>
-               <h5 style={{fontSize: '16px'}}>{user.role==="owner"?<CrownOutlined style={{color: 'yellow',fontSize: '17px',paddingRight: '2px'}}></CrownOutlined>:''}
+                  
+                <div className="collectinbox" style={user.avatar===null?{textAlign: 'left',marginLeft: '-1%',border: 'none',marginTop: '5%'}:{textAlign: 'left',marginLeft: '-1%',border: 'none',marginTop: '1%'}}>
+               <Link to={'/Profile/'+user.user}> <h5 style={{fontSize: '16px' }}>{user.user}</h5></Link>
+               <Row>
+                 <Col span={12}>
+                <h5 style={{fontSize: '16px'}}>{user.role==="owner"?<CrownOutlined style={{color: 'yellow',fontSize: '17px',paddingRight: '2px'}}></CrownOutlined>:''}
               {user.role==="editor"?<EditOutlined style={{color: '#f19813',fontSize: '17px',paddingRight: '3px'}}></EditOutlined>:''}
               {user.role==="visitor"?<EyeOutlined style={{color: '#5ef113',fontSize: '15px',paddingRight: '3px'}}></EyeOutlined>:''}             
               {user.user===localStorage.getItem('user')?this.power=user.role:user.role}</h5>
-              {localStorage.getItem('user')===user.user?'':<Row>
-                <Col span={12}>
-                <Button hidden={this.state.power==='visitor'||user.role==='owner'||(user.role==='editor'&&this.state.power==='editor')} style={{backgroundColor: 'green',border: 'none',color: 'white'}} onClick={()=>this.Promote(user,item)}>promote</Button>
-                </Col>
-                <Col span={12}>
-                <Button  hidden={this.state.power==='visitor'||user.role==='owner'||(user.role==='editor'&&this.state.power==='editor')} style={{backgroundColor: 'red',border: 'none',color: 'white'}} onClick={()=>this.Removeuser(user,item)}>remove</Button>
+              </Col>
+              <Col span={12}>
+                <Button  hidden={this.state.power==='visitor'||user.role==='owner'||this.state.power==='editor'} style={{backgroundColor: 'red',border: 'none',color: 'white',width: '80%'}} onClick={()=>this.Removeuser(user,item)}> remove </Button>
 
                 </Col>
+              </Row>
+              {localStorage.getItem('user')===user.user?'':<Row style={{marginTop: '2%'}}>
+                <Col span={12}>
+                <Button hidden={this.state.power==='visitor'||user.role==='owner'||this.state.power==='editor'} style={{backgroundColor: 'green',border: 'none',color: 'white'}} onClick={()=>this.Promote(user,item)}>promote</Button>
+                </Col>
+                <Col span={12}>
+              <Button hidden={this.state.power==='visitor'||user.role==='owner'||user.role==='visitor'||this.state.power==='editor'} style={{backgroundColor: '#ff5200',border: 'none',color: 'white'}} onClick={()=>this.Demote(user,item)}>demote</Button>
+              </Col>
                 </Row>}  
             </div>
                 </Col>
@@ -366,62 +408,7 @@ class Collection extends React.Component {
 
             </Col>
           </Row>
-          :<h5>{this.state.power}</h5>}
-          <div style={{marginTop: '3%',marginLeft: '1%',borderTop: '1px solid gray',paddingBottom: '2%'}}>
-            <Row>
-              <Col span={24}>
-              <h5 style={{fontSize: '20px' ,float: 'left'}}>Requests:</h5>
-
-              </Col>
-              </Row>
-              {this.state.requests.map(req=>(
-                <Row style={{marginTop: '2%'}}>
-                <Col span= {4} style={{float: 'left',marginLeft: '1%'}}>
-                   <h5 style={{fontSize: '17px' }}>Name : {req.name}</h5>
-
-                </Col>
-                <Col span={4}>
-                <h5 style={{fontSize: '17px' }}>Method : {req.http_method}</h5>
-                </Col>
-                <Col span={6}>
-                <Button style={{backgroundColor: '#1890ff',color: 'white',border: 'none',width: '60%'}} onClick={()=>{this.addClick(req)}}>Open Request</Button>
-
-
-                </Col>
-                </Row>
-              ))}
-            </div>
-            <Row style={{marginTop: '3%',marginLeft: '1%',paddingBottom: '5%'}}>
-          <Col span={6} style={{marginTop: '3%'}}>
-          <h5 style={{fontSize: '20px' }}>Add a new request:</h5>
-          </Col>
-          <Col span={4} style={{marginTop: '3%'}}>
-            <Input onChange={this.thirdChange} placeholder="request Name" style={{width: '80%'}}/>            
-          </Col>
-          <Col span={4} style={{marginTop: '3%'}}>
-            <Input onChange={this.changeType} placeholder="Request type" style={{width: '80%'}}/>            
-          </Col>
-          <Col span={6} style={{marginTop: '3%'}}>
-          <Input  style={{width: '100%'}}  placeholder='Url' onChange={this.onChangeInputUrl} />
-                    
-          </Col>
-          <Col span={4} style={{marginTop: '3%'}}>
-            <Button style={{backgroundColor: '#1890ff',color: 'white',border: 'none',width: '60%'}} onClick={()=>{this.Addreq(item)}}>Add Request</Button>
-
-            </Col>
-
-            </Row>
-          <Row style={{marginLeft: '1%',borderTop: '1px solid gray',paddingBottom: '5%'}}>
-            <Col span={8}>
-            </Col>
-            <Col span={8} style={{marginTop: '3%'}}>
-            {this.state.power==="owner"?
-            <Button style={{backgroundColor: '#cc7d1c',color: 'white',border: 'none',width: '60%'}} onClick={()=>{this.deleCollection(item)}}>Delete</Button>
-            :
-            <Button style={{backgroundColor: '#cc7d1c',color: 'white',border: 'none',width: '60%'}} onClick={()=>{this.leaveCollection(item)}}>Leave</Button>
-          }
-            </Col>
-          </Row>
+          :<h5></h5>}
           
         </div>
      
