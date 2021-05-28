e80bb6da54d1e621b961d736f710221e9ce2b213
mohammadhosseinkarimian <47971866+mohammadhosseinkarimian@users.noreply.github.com>
2021-05-29 02:09:19 +0430

    [edit] create edges in scenario

--- src/Scenario/Scenario.js
+++ src/Scenario/Scenario.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useCallback  } from 'react';
+import React, { useState, useRef, useCallback, useEffect  } from 'react';
 import ReactFlow, {
     ReactFlowProvider,
     addEdge,
@@ -7,6 +7,7 @@ import ReactFlow, {
 } from 'react-flow-renderer';
 import ConnectionLine from './ConnectionLine';
 import './dnd.css';
+import '../style/design.scss'
 import Sidebar from './Sidebar';
 import localforage from 'localforage';
 import {SettingTwoTone, ToolOutlined} from '@ant-design/icons';
@@ -14,7 +15,7 @@ import './layouting.css';
 import inputNode from './CustomInputNode';
 import defaultNode from './CustomDefaultNode';
 import outputNode from './CustomOutputNode';
-import { Input ,Modal,Button} from 'antd';
+import { Input ,Modal,Button,Row,Col,message} from 'antd';
 
 
 const initialElements = [
@@ -43,18 +44,40 @@ const nodeTypes = {
     outputNode: outputNode,
   };
   
-
+let edge_data= []
 
 const flowKey = 'example-flow';
 const DnDFlow = () => {
+  
+    const [condition_set, setcondition_set] = useState("");
+    const [source_set, setsource_set] = useState("");
+    //localforage.setItem("edge_data",[])
+    //const [edge_data, setedge_data] = useState([]);
+    const [target_set, settarget_set] = useState("");
     const [visible_edge, setvisible_edge] = useState(false);
+    const [visible_edge_edit, setvisible_edge_edit] = useState(false);
     const edgeOk = () => {
+    const data={source: source_set ,target: target_set , condition: condition_set}
+    //const newone = localforage.getItem("edge_data")
+    edge_data.push(data)
+    alert(JSON.stringify(edge_data))
     setvisible_edge(false) ;
+    setsource_set("");
+    settarget_set("");
+    setcondition_set("");
+    };
+    const edgeEditOk = () => {
+        setvisible_edge_edit(false) ;
     };
     const showModalEdge = () => {
         
     setvisible_edge(true);
     };
+    const showModalEdgeEdit = useCallback(() => {
+            setvisible_edge_edit(true);
+        
+      });
+
     const reactFlowWrapper = useRef(null);
     const [reactFlowInstance, setReactFlowInstance] = useState(null);
     const [elements, setElements] = useState(initialElements);
@@ -66,17 +89,33 @@ const DnDFlow = () => {
             ...params,
             animated: true,
             arrowHeadType: 'arrow',
-            id: '12',
+            id: params.source+","+params.target,
             style: {strokeWidth:3},
             
             // label:'Setting',
             // labelStyle:{fill:'#fff',fontWeight: 800},
             // labelShowBg:false,
         };
+        setsource_set(params.source)
+        settarget_set(params.target)
+        conditionEmpty();
         showModalEdge();
-        alert(JSON.stringify(params))
         setElements((els) => addEdge(params, els));
     }
+    const conditionSet = (e) => {
+        setcondition_set(e.target.value);
+      };
+    
+    function editCondition(edge,e){
+        const x=edge
+        const index = edge_data.indexOf(edge);
+        edge_data.splice(index,1)
+        const new_data = {source: x.source , target: x.target , condition : e.target.value}
+        edge_data.push(new_data)
+      };
+    const conditionEmpty = () => {
+        setcondition_set("");
+      };  
 
   const onEdge = (params) => {
 
@@ -85,10 +124,29 @@ const DnDFlow = () => {
     const prevent = (e) => {
         e.preventDefault()
         }
-    const onElementsRemove = (elementsToRemove) =>
+    const onElementsRemove = (elementsToRemove) =>{
+        elementsToRemove.forEach(e=>{
+            try{
+
+                const new_source = e.source
+                const new_target = e.target
+                edge_data.forEach(element => {
+                    if(element.source===new_source && element.target===new_target)
+                    {
+                      const ind = edge_data.indexOf(element);
+                      edge_data.splice(ind,1)
+                    }
+                });
+          }
+          catch{}
+        })
+        
         setElements((els) => removeElements(elementsToRemove, els));
+    
+    }
 
         const onLoad = (_reactFlowInstance) =>{
+        
         setReactFlowInstance(_reactFlowInstance);
 
         }
@@ -163,12 +221,51 @@ const DnDFlow = () => {
                 title="Set condition"
                 style={{height: '36vh'}}
                 footer={[
-                <Button key="ok" className="btn btn-primary" onClick={edgeOk}>Set
+                <Button key="ok" className="btn btn-primary" onClick={edgeOk} >Set
                 </Button>
                 ]}
             >
         <div style={{alignContent: 'center' ,marginLeft: 'auto',marginRight: 'auto',alignItems: 'center',textAlign: 'center'}}>
-        <h5 >input</h5>
+        <Input
+                  required
+                  name="condition"
+                  placeholder="condition for set"
+                  onChange={conditionSet}
+                />
+        </div>
+      </Modal>
+      <Modal
+                visible={visible_edge_edit}
+                title="Edit Edges"
+                style={{height: '36vh'}}
+                footer={[
+                <Button key="ok" className="btn btn-primary" onClick={edgeEditOk} >Done
+                </Button>
+                ]}
+            >
+        <div style={{alignContent: 'center' ,marginLeft: 'auto',marginRight: 'auto',alignItems: 'center',textAlign: 'center'}}>
+        {edge_data.length===0?
+        <h5>There is no edge in this scenario</h5>:
+        edge_data.map(e=>(
+            <Row>
+                <Col span={4}><h5>Source : {e.source}</h5></Col>
+                <Col span={4}><h5>Target : {e.target}</h5></Col>
+                <Col span={4}><h5>Condition : </h5></Col>
+
+                <Col span = {8}><Input
+                  required
+                  name="edit_condition"
+                  defaultValue={e.condition}
+                  onChange={(er) => editCondition(e,er)}
+                  style={{height: '3vh'}}
+                />
+                </Col>
+                <Col span={4}><Button style={{backgroundColor: '#1890ff',color: 'white',
+                border: 'none',lineHeight: '0.5',width: '90%',height: '3vh'}}>Edit</Button></Col>
+                
+            </Row>
+        ))
+        }
         </div>
       </Modal>
             <ReactFlowProvider>
@@ -191,7 +288,7 @@ const DnDFlow = () => {
                     </ReactFlow>
                 </div>
                 
-                <Sidebar onSave={onSave} onRestore={onRestore} />
+                <Sidebar onSave={onSave} onRestore={onRestore} showModalEdgeEdit={showModalEdgeEdit}/>
                 <div className="controls">
         </div>
             </ReactFlowProvider>
